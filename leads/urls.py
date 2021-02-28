1892db9633f37ed0614fcecacf11b28a8a991fa6
Alex <alexdeathway@gmail.com>
2021-02-28 22:12:25 +0530

    lead detail and update

--- leads/urls.py
+++ leads/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from .views import (
                     index,
-                    leadlist,
-                    CreateLead
+                    LeadList,
+                    LeadCreate,
+                    LeadDetail,
+                    LeadUpdate,
                     )        
 
 urlpatterns=[
     path("",index),
-    path("leadlist/",leadlist),
-    path("create/",CreateLead)
+    path("leadlist/",LeadList,name="leadlist"),
+    path("create/",LeadCreate,name="leadcreate"),
+    path("<int:pk>",LeadDetail),
+    path("<int:pk>/update",LeadUpdate)
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import LeadModel
 from .forms import LeadCreationForm
@@ -9,7 +9,7 @@ def index(request):
     print(request)
     return HttpResponse("Received")
 
-def leadlist(request):
+def LeadList(request):
      leads=LeadModel.objects.all()
      context={
          "leads":leads
@@ -17,8 +17,38 @@ def leadlist(request):
      return render(request,"leads/leads_list.html",context)
 
 
-def CreateLead(request):
+def LeadCreate(request):
+    if request.POST:
+        form =LeadCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("leadlist")
+
     context={
         "LeadCreationForm":LeadCreationForm
     }
     return render(request,"leads/leads_create.html",context)
+
+
+def LeadDetail(request,pk):
+    lead=LeadModel.objects.get(id=pk)
+    context={
+        "lead":lead
+    }
+    return render(request,"leads/leads_detail.html",context)
+
+
+def LeadUpdate(request,pk):
+    lead=LeadModel.objects.get(id=pk)
+    form=LeadCreationForm(instance=lead)
+    if request.POST:
+        form =LeadCreationForm(request.POST,instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("leadlist")
+    context={
+        #"lead":lead,
+        "form":form,
+        "lead":lead,
+            }
+    return render(request,"leads/leads_update.html",context)        
