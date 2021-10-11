d985b5f9e61fada95ae6724fdbc7cda8c34f2a0e
alexdeathway <alexdeathway@gmail.com>
2021-10-11 13:57:16 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,27 @@
+﻿# Crm
+
+
+
+## Setup Development Environment
+
+ 1. create Virtual environment
+ 
+>  virtualenv cenv
+
+ 2. Install required packages
+
+>  pip install -r requirements.txt
+
+3. Create .env file in directory /crm/crm/   with following:
+ 
+> DEBUG=True 
+>  SECRET_KEY= 
+
+4. set Environment variable READ_ENV_FILE as True
+ 
+
+> export READ_ENV_FILE=True
+
+
+  
+
--- crm/settings.py
+++ crm/settings.py
@@ -32,8 +32,7 @@ if READ_ENV_FILE:
 # SECURITY WARNING: keep the secret key used in production secret!
 
 DEBUG=env('DEBUG')
-SECRET_KEY=env('SECRET_KEY')   
-print(DEBUG,SECRET_KEY)          
+SECRET_KEY=env('SECRET_KEY')           
 
 #SECRET_KEY = ''
 
@@ -95,16 +94,25 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': env('DATABASE_NAME'),
-       'USER': env('DATABASE_USER'),
-       'PASSWORD': env('USER_PASSWORD'),
-       'HOST': env('HOSTNAME'),
-       'PORT': env('PORT'),
-   }
+if DEBUG:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+else:
+    DATABASES = {
+    'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('USER_PASSWORD'),
+        'HOST': env('HOSTNAME'),
+        'PORT': env('PORT'),
+        }
+    }
 
 
 # Password validation
